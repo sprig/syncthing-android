a01ceb26343062cf08facb8654a40dd64698beb1
Catfriend1 <Catfriend1@users.noreply.github.com>
2019-07-24 08:40:35 +0200

    build-syncthing.py: Set version to v1.2.1-preview4-mtime

--- syncthing/build-syncthing.py
+++ syncthing/build-syncthing.py
@@ -362,6 +362,7 @@ syncthingVersion = subprocess.check_output([
     'describe',
     '--always'
 ]).strip();
+syncthingVersion = 'v1.2.1-preview4-mtime';
 syncthingVersion = syncthingVersion.decode().replace("rc", "preview");
 
 print('Cleaning go-build cache')
